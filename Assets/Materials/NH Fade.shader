181fba2a8984cc04016c01774f40fb4c25adbf27
LAPTOP-0816I1H1\ATOMTWIST <atomtwist@gmail.com>
2018-02-24 14:41:37 +0100

    added Fadecontroller Prefab

---
+++ Assets/Materials/NH Fade.shader
@@ -0,0 +1,108 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Nothing Happens/NH Fade"
+{
+    Properties
+    {
+        [HideInInspector] _MainTex ("Screen", 2D) = "white" {}
+        [HideInInspector] _CubeTex ("Main Texture", CUBE) = "black" {}
+        [Space(20)]
+        [HideInInspector] _UseTexture ("Use Texture", Float) = 0.0
+        _Color ("Color/Tint", Color) = (1,1,1,1)
+    }
+
+    SubShader {
+        Tags
+        {
+            "IgnoreProjector" = "True"
+            "Queue" = "Overlay"
+            "CanUseSpriteAtlas" = "True"
+            "ForceNoShadowCasting" = "True"
+        }
+
+        LOD 100
+
+        Pass
+        {
+            Name "FORWARD"
+            Tags {
+                "LightMode" = "ForwardBase"
+            }
+
+            Blend SrcAlpha OneMinusSrcAlpha
+            Cull Off
+            ZWrite Off
+            ZTest Always
+
+            CGPROGRAM
+
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            #pragma target 4.6
+
+
+            // public properties
+            uniform sampler2D _MainTex;
+            uniform float _UseTexture;
+            uniform samplerCUBE _CubeTex;
+            uniform float4 _Color;
+
+
+            // structs
+            struct appdata {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                float4 worldPos : TEXCOORD1;
+            };
+
+            // vertex
+            v2f vert (appdata v)
+            {
+                v2f o;
+
+                o.uv = v.uv;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+
+                return o;
+            }
+
+            // fragment
+            // v2f_img for image effects
+            float4 frag(v2f i) : COLOR
+            {
+                // calculate view direction
+                float3 viewDirection;
+                viewDirection.x = _WorldSpaceCameraPos.x - i.worldPos.x;
+                viewDirection.y = i.worldPos.y - _WorldSpaceCameraPos.y;
+                viewDirection.z = i.worldPos.z - _WorldSpaceCameraPos.z;
+
+                // fog cubemap
+                float4 cube = texCUBE(_CubeTex, viewDirection);
+                cube.rgb = lerp(1, cube.rgb, _UseTexture); // toggle cubemap
+
+                // screen texture
+                //float4 screen = tex2D(_MainTex, i.uv);
+
+                float4 c;
+                //c.rgb = lerp(screen, _Color.rgb * cube.rgb, _Color.a);
+                c.rgb = _Color.rgb * cube.rgb;
+                c.a = _Color.a;
+
+                return c;
+            }
+
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Materials/NH_Snow 1/FadeScreen.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using Kaae;
+using UnityEngine;
+
+public class FadeScreen : Singleton<FadeScreen>
+{
+    public Cubemap cubemap;
+
+    public bool Colliding;
+
+    private Material _mat;
+    private Renderer _renderer;
+    private Coroutine _fadeRoutine;
+    private Color _color = Color.clear;
+    private Color _collideColor = Color.black;
+    public bool _covered = false;
+    public bool fadeInOnEnable;
+
+    private void OnEnable()
+    {
+        _renderer = GetComponent<Renderer>();
+        _mat = _renderer.material;
+        if (fadeInOnEnable)
+        {
+            _mat.color = Color.black;
+            Fade(Color.clear,5);
+        }
+
+        if (cubemap != null)
+        {
+            _mat.SetInt("_UseTexture", 1);
+            _mat.SetTexture("_CubeTex", cubemap);
+        }
+
+        // automatically parent under main camera
+        transform.parent = Camera.main.transform;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    public void Fade(Color color, float time = 1f, Texture texture = null)
+    {
+
+        if (texture)
+        {
+            _mat.SetTexture("_CubeTex", texture);
+            _mat.SetInt("_UseTexture", 1);
+        }
+
+        _mat.SetInt("_UseTexture", _mat.GetTexture("_CubeTex") == null ? 0 : 1);
+
+        if (_mat.color.a < 0.001f)
+        {
+            var c = color;
+            c.a = 0;
+            _mat.color = c;
+        }
+
+        if (_fadeRoutine != null)
+            StopCoroutine(_fadeRoutine);
+
+        _fadeRoutine = StartCoroutine(FadeFromTo(_mat.color, color, time));
+    }
+
+    IEnumerator FadeFromTo(Color from, Color to, float time)
+    {
+        var fade = true;
+        var d = 1 / time;
+        var t = 0f;
+        var startTime = Time.time;
+
+        var color = from;
+        _covered = false;
+
+        while (fade)
+        {
+            yield return null;
+
+            t = d * (Time.time - startTime);
+
+            if (t > 0.999f)
+            {
+                t = 1f;
+                fade = false;
+                color = to;
+            }
+            else
+            {
+                color = Color.Lerp(from, to, Mathf.SmoothStep(0.0f, 1.0f, t));
+            }
+
+
+            // if fading out (black)
+            if (Colliding)
+            {
+                if (from.a < to.a)
+                {
+                    var a = color.a > _collideColor.a ? color.a : _collideColor.a;
+                    color = Color.Lerp(_collideColor, color, t);
+                    color.a = a;
+                }
+                // else fading in (clear)
+                else
+                {
+                    var a = color.a < _collideColor.a ? _collideColor.a : color.a;
+                    color = Color.Lerp(_collideColor, color, t);
+                    color.a = a;
+                }
+            }
+
+            _mat.color = color;
+        }
+
+        _covered = to.a > 0.99f;
+
+        _fadeRoutine = null;
+    }
+
+    public void Clear()
+    {
+        if (_fadeRoutine != null)
+            StopCoroutine(_fadeRoutine);
+
+        var color = _mat.color;
+        color.a = 0;
+        _mat.color = color;
+
+        _covered = false;
+    }
+
+    public void Cover(Color? color = null)
+    {
+        if (_fadeRoutine != null)
+            StopCoroutine(_fadeRoutine);
+
+        _covered = true;
+
+        _mat.color = color ?? Color.black;
+    }
+
+    public void Collide(Color color)
+    {
+        // only fixed when not already fading
+        if (_fadeRoutine == null && !_covered)
+        {
+            var a = color.a;
+            var c = _mat.color;
+            c = Color.Lerp(c, color, 0.5f * Time.deltaTime);
+            c.a = a;
+            _mat.color = c;
+        }
+        // otherwise can only be tweaked to less
+        else
+        {
+            var a = color.a;
+            _collideColor = Color.Lerp(_mat.color, color, 0.5f * Time.deltaTime);
+            _collideColor.a = a;
+        }
+    }
+
+    [DebugButton]
+    private void TestFadeBlack() { Fade(Color.black, 5f); }
+
+    [DebugButton]
+    private void TestFadeClear() { Fade(Color.clear, 5f); }
+
+    [DebugButton]
+    private void TestClear() { Clear();}
+
+    [DebugButton]
+    private void TestCover() { Cover();}
+
+}
